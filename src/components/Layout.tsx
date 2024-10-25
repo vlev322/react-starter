3559a5cdd28ea3adbd5e1bfd2bdc5d78ec0735e0
Vluldislav <29085049+vlev322@users.noreply.github.com>
2024-10-25 09:43:56 +0300

    removed extra header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { AppSidebar } from '@/components/AppSidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
@@ -8,22 +8,6 @@ export default function Layout() {
   return (
     <SidebarProvider>
       <div className="flex flex-col min-h-screen">
-        <header className="p-4">
-          <nav>
-            <ul className="flex gap-4">
-              <li>
-                <Link to="/" className="text-blue-600 hover:underline">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/profiles" className="text-blue-600 hover:underline">
-                  Profiles
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
         <div className="flex flex-grow">
           <AppSidebar />
           <main className="flex-grow p-4">
